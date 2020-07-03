5aa50064ca75d5b5d957417d9e17e2ee9820791b
cpulover <justacpulover@gmail.com>
2020-07-03 03:03:35 +0200

    Setup DAO and controller

--- src/com/cpulover/springdemo/dao/CustomerDAOImpl.java
+++ src/com/cpulover/springdemo/dao/CustomerDAOImpl.java
@@ -6,10 +6,12 @@ import org.hibernate.Session;
 import org.hibernate.SessionFactory;
 import org.hibernate.query.Query;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Repository;
 import org.springframework.transaction.annotation.Transactional;
 
 import com.cpulover.springdemo.entity.Customer;
 
+@Repository
 public class CustomerDAOImpl implements CustomerDAO {
 	@Autowired
 	private SessionFactory sessionFactory;
